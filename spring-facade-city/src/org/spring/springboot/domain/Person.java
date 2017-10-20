a2fd1417d870e553520b5eab9df7aa2cc147c031
sam <lvle_sz@gcl-powner.com>
2017-10-20 09:39:31 +0800

    commit

---
+++ spring-facade-city/src/org/spring/springboot/domain/Person.java
@@ -0,0 +1,39 @@
+package org.spring.springboot.domain;
+
+import java.io.Serializable;
+
+public class Person implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+	private String sex;
+	public Person(){}
+	public Person(String id, String name, String sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sex = sex;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
